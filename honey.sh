eb1f0babd80ce881ec8c9bc23e67c5544e4ad251
koRajesh <118733471+koRajesh@users.noreply.github.com>
2022-11-21 23:12:54 +0530

    Update honey.sh

--- honey.sh
+++ honey.sh
@@ -1,2 +1,1 @@
 docker run --restart unless-stopped honeygain/honeygain -tou-accept -email thoufeeqisin@gmail.com -pass Thoufeeq$369 -device c2ptech
-docker run --restart unless-stopped honeygain/honeygain -tou-accept -email thoufeeqisin@gmail.com -pass Thoufeeq$369 -device c2ptech1
