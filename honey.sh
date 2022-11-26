7068eb85af3941cccee0d5c3ecc0f3a69d932527
koRajesh <118733471+koRajesh@users.noreply.github.com>
2022-11-26 13:17:36 +0530

    Update honey.sh

--- honey.sh
+++ honey.sh
@@ -1,1 +1,7 @@
-sudo docker run --restart unless-stopped honeygain/honeygain -tou-accept -email thoufeeqisin@gmail.com -pass Thoufeeq$369 -device honeynode
+
+wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz > /dev/null
+mkdir OtohitsApp
+cd OtohitsApp
+echo "/login:20a3792a-11f7-41d0-8eb9-39e9bb48975d" > otohits.ini
+tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
+./otohits-app
